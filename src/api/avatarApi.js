7f4ecf50156e39daa64cb2993a34c4aa6bd8e2f8
Nouran Sherif <nouransherif901@gmail.com>
2026-06-12 00:46:26 +0300

    feat: avatar generation page with auth, try-on integration, and mobile lang fix

--- src/api/avatarApi.js
+++ src/api/avatarApi.js
@@ -1,14 +1,4 @@
 import api from "./axiosInstance";
 
-const MOCK_AVATAR = import.meta.env.VITE_MOCK_AVATAR === "true";
-
-const MOCK_IMAGE = "https://i.pravatar.cc/400?u=avatar";
-
-export const generateAvatarApi = (data) => {
-  if (MOCK_AVATAR) {
-    return Promise.resolve({
-      data: { avatar: { image_url: MOCK_IMAGE } },
-    });
-  }
-  return api.post("/avatars", data);
-};
+export const generateAvatarApi = (data) =>
+  api.post("/avatars", data);
