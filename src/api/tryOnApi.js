194434a812b6347b9ceab1b64437ed7054266031
Ahmed-Azaz <105168908+Ahmed-Azaz@users.noreply.github.com>
2026-06-21 06:47:49 +0300

    fix: api keys from header

--- src/api/tryOnApi.js
+++ src/api/tryOnApi.js
@@ -1,12 +1,9 @@
 import api from "./axiosInstance";
 
-const KIE_API_KEY = import.meta.env.VITE_KIE_API_KEY || "";
-
 export const virtualTryOnApi = (formData) =>
   api.post("/virtual-tryon", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
-      ...(KIE_API_KEY && { "x-kie-api-key": KIE_API_KEY }),
     },
   });
 
@@ -14,6 +11,5 @@ export const virtualTryOnOutfitApi = (formData) =>
   api.post("/virtual-tryon/outfit", formData, {
     headers: {
       "Content-Type": "multipart/form-data",
-      ...(KIE_API_KEY && { "x-kie-api-key": KIE_API_KEY }),
     },
   });
